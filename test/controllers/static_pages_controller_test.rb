d186c2015659bfc3f3379677e961f68ee46d3b27
alex shyaka <alexshyaka@therailsshop.club>
2017-11-20 09:42:36 +0300

    add static pages and test them using minitest and guard

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,17 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
+    
     assert_response :success
+    assert_select "title", "home | The ruby on rails tutorial sample app"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title" ,"help | The ruby on rails tutorial sample app"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "about | The ruby on rails tutorial sample app"
+  end
+    test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "contact | The ruby on rails tutorial sample app"
   end
 
 end
