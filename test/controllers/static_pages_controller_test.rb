7d226921bff7868d9f7cfc96716de392a93021f5
alex shyaka <alexshyaka@therailsshop.club>
2017-11-21 18:21:31 +0300

    small change in the controller test file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,18 +11,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title" ,"Help|Ruby on Rails Tutorial"
+    assert_select "title" ,"Help | Ruby on Rails Tutorial"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About|Ruby on Rails Tutorial"
+    assert_select "title", "About | Ruby on Rails Tutorial"
   end
     test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact|Ruby on Rails Tutorial"
+    assert_select "title", "Contact | Ruby on Rails Tutorial"
   end
 
 end
