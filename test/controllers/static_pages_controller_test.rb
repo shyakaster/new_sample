3c94b2c954d3b45485b3ed9f8207446043ee6bd1
alex shyaka <alexshyaka@therailsshop.club>
2017-11-28 23:15:43 +0300

    removed langauge server gem which was causing errors with partials and stack level too deep issues

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get root_path
+    get root_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial"
   end
 
   test "should get help" do
-    get help_path
+    get help_url
     assert_response :success
     assert_select "title" ,"Help | Ruby on Rails Tutorial"
   end
 
   test "should get about" do
-    get about_path
+    get about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial"
   end
     test "should get contact" do
-    get contact_path
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial"
   end
