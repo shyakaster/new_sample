56dcb2383e57aef924b85f5d189f4fb35c7ea3ed
alex shyaka <alexshyaka@therailsshop.club>
2017-11-28 10:05:00 +0300

    partial issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
 
@@ -8,6 +9,5 @@ class StaticPagesController < ApplicationController
   def about
   end
   def contact
-    
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial"
   end
 
   test "should get help" do
-    get help_url
+    get help_path
     assert_response :success
     assert_select "title" ,"Help | Ruby on Rails Tutorial"
   end
 
   test "should get about" do
-    get about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial"
   end
     test "should get contact" do
-    get contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial"
   end
